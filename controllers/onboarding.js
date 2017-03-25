634e7030ae22c93a3ec1ebb147256ff20df0e021
Josh Martin <joshmartin5001@gmail.com>
2017-03-25 04:24:40 -0400

    made jade file

--- controllers/onboarding.js
+++ controllers/onboarding.js
@@ -36,7 +36,9 @@ exports.userOnboardingGet = function(req, res) {
   if (req.user) {
     return res.redirect('/');
   }
-  res.render('account/forgot', {
-    title: 'Log in'
+
+
+  res.render('account/welcome', {
+    title: 'Welcome'
   });
 };
